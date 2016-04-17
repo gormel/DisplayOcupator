247704d921467a157d92046f650f3f73e1804f00
Roman <gremkil@gmail.com>
2016-04-18 01:32:58 +0300

    *доделал

--- DisplayOcupator/Form1.Designer.cs
+++ DisplayOcupator/Form1.Designer.cs
@@ -28,10 +28,19 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.listBox1 = new System.Windows.Forms.ListBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
 			this.flowLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.flowLayoutPanel2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// listBox1
@@ -41,7 +50,8 @@
 			this.listBox1.FormattingEnabled = true;
 			this.listBox1.Location = new System.Drawing.Point(12, 12);
 			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(150, 316);
+			this.listBox1.Size = new System.Drawing.Size(150, 667);
+			this.listBox1.Sorted = true;
 			this.listBox1.TabIndex = 0;
 			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
 			// 
@@ -61,24 +71,104 @@
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.flowLayoutPanel1.AutoScroll = true;
 			this.flowLayoutPanel1.Controls.Add(this.label1);
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(168, 12);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(168, 39);
 			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(636, 316);
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(829, 643);
 			this.flowLayoutPanel1.TabIndex = 2;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "rare",
+            "common",
+            "uncommon",
+            "mythic",
+            "-"});
+			this.comboBox1.Location = new System.Drawing.Point(208, 12);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(789, 21);
+			this.comboBox1.TabIndex = 3;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(168, 15);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(34, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Rarity";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.numericUpDown1.Location = new System.Drawing.Point(1074, 12);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(139, 20);
+			this.numericUpDown1.TabIndex = 5;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            36,
+            0,
+            0,
+            0});
+			this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+			// 
+			// flowLayoutPanel2
+			// 
+			this.flowLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.flowLayoutPanel2.Controls.Add(this.label3);
+			this.flowLayoutPanel2.Location = new System.Drawing.Point(1003, 39);
+			this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+			this.flowLayoutPanel2.Size = new System.Drawing.Size(210, 643);
+			this.flowLayoutPanel2.TabIndex = 6;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(3, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(35, 13);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "label3";
+			// 
+			// label4
+			// 
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(1006, 14);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(62, 13);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Pack count";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(816, 342);
+			this.ClientSize = new System.Drawing.Size(1225, 696);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.flowLayoutPanel2);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1);
 			this.Controls.Add(this.flowLayoutPanel1);
 			this.Controls.Add(this.listBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "Display ocupator 6000";
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.flowLayoutPanel1.ResumeLayout(false);
 			this.flowLayoutPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.flowLayoutPanel2.ResumeLayout(false);
+			this.flowLayoutPanel2.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -87,6 +177,12 @@
 		private System.Windows.Forms.ListBox listBox1;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
 	}
 }
 
--- DisplayOcupator/Form1.cs
+++ DisplayOcupator/Form1.cs
@@ -1,22 +1,23 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Globalization;
 using System.Linq;
-using System.Net;
+using System.Net.Http;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Runtime.CompilerServices;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using HtmlAgilityPack;
 using Newtonsoft.Json;
 
 namespace DisplayOcupator
 {
 	public partial class Form1 : Form, INotifyPropertyChanged
 	{
-		private class ListBoxItem
+		internal class ListBoxItem
 		{
 			public string SetName { get; set; }
 			public string Url { get; set; }
@@ -31,13 +32,49 @@ namespace DisplayOcupator
 		{
 			public string Name { get; set; }
 			public string Price { get; set; }
+			public string Rarity { get; set; }
 		}
 
-		List<CardInfo> cardInfos = new List<CardInfo>();
+		ConcurrentQueue<CardInfo> cardInfos = new ConcurrentQueue<CardInfo>();
 
 		public string CardList
 		{
-			get { return cardInfos.Any() ? cardInfos.Select(c => c.Name + ": " + c.Price).Aggregate((a, b) => a + Environment.NewLine + b) : ""; }
+			get
+			{
+				return cardInfos.Any() ? 
+					cardInfos.Where(c => string.IsNullOrEmpty(comboBox1.Text) || comboBox1.Text == "-" || c.Rarity == comboBox1.Text)
+							 .Select(c => c.Name + "(" + c.Rarity + ")" + ": " + c.Price)
+							 .Aggregate("", (a, b) => a + Environment.NewLine + b)
+					: "";
+			}
+		}
+
+		public string MainResult
+		{
+			get
+			{
+				if (cardInfos.All(i => i.Rarity != "rare") || cardInfos.All(i => i.Rarity != "uncommon") || cardInfos.All(i => i.Rarity != "common"))
+					return "";
+				
+				var rare = cardInfos.Where(i => i.Rarity == "rare").Average(i => Convert(i.Price));
+				var uncommon = cardInfos.Where(i => i.Rarity == "uncommon").Average(i => Convert(i.Price));
+				var common = cardInfos.Where(i => i.Rarity == "common").Average(i => Convert(i.Price));
+				var mythic = cardInfos.Any(i => i.Rarity == "mythic") ? cardInfos.Where(i => i.Rarity == "mythic").Average(i => Convert(i.Price)) : 0m;
+				var busters = numericUpDown1.Value;
+				return
+					string.Format("Mythic: {5:00.00}({6:00.00}){0}Rare: {1:00.00}({7:00.00}){0}Uncommon: {2:00.00}({8:00.00}){0}Common: {3:00.00}({9:00.00}){0}Overall Rare and Mythic: {4:00.00}",
+						Environment.NewLine, rare*busters, uncommon*busters*3, common*busters*11, (rare * 0.875m + mythic * 0.125m) * busters, mythic * busters / 8,
+						mythic, rare, uncommon, common);
+			}
+		}
+
+		private decimal Convert(string price)
+		{
+			if (string.IsNullOrEmpty(price))
+				return 0;
+			if (price.StartsWith("$"))
+				price = price.Substring(1);
+			return decimal.Parse(price, CultureInfo.InvariantCulture);
 		}
 
 		public Form1()
@@ -45,77 +82,99 @@ namespace DisplayOcupator
 			InitializeComponent();
 
 			label1.DataBindings.Add("Text", this, "CardList");
+			label3.DataBindings.Add("Text", this, "MainResult");
 		}
 
-		private void Form1_Load(object sender, EventArgs e)
+		private async void Form1_Load(object sender, EventArgs e)
 		{
-			var client = new WebClient();
-			client.Encoding = new UTF8Encoding();
-			var listBoxItems = new List<ListBoxItem>();
-			var setsData = client.DownloadString("https://api.deckbrew.com/mtg/sets");
+			var client = new HttpClient();
+			var setsData = await client.GetStringAsync("https://api.deckbrew.com/mtg/sets");
 			dynamic result = JsonConvert.DeserializeObject(setsData);
 			foreach (dynamic setData in result)
 			{
-				listBoxItems.Add(new ListBoxItem() { SetName = setData.name, Url = setData.cards_url });
+				listBox1.Items.Add(new ListBoxItem() { SetName = setData.name, Url = setData.cards_url });
 			}
-			listBox1.Items.AddRange(listBoxItems.ToArray());
 			client.Dispose();
 		}
 
-		private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+		private async void listBox1_SelectedIndexChanged(object sender, EventArgs e)
 		{
 			var listItem = listBox1.SelectedItem as ListBoxItem;
 			if (listItem == null)
 				return;
-			Text = listItem.Url;
-			LoadCards(listItem.Url, listItem.SetName);
-		}
-
-		private void LoadCards(string url, string setName)
-		{
-			cardInfos.Clear();
-			var cardsClient = new WebClient();
-			cardsClient.DownloadStringCompleted += CardsClientOnDownloadStringCompleted;
-			cardsClient.DownloadStringAsync(new Uri(url), new { page = 0, url, setName });
-			OnPropertyChanged("CardList");
-		}
-
-		private void CardsClientOnDownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
-		{
-			dynamic result = JsonConvert.DeserializeObject(e.Result);
-			int found = 0;
-			dynamic userInfo = e.UserState;
-			foreach (var cardData in result)
+			try
 			{
-				var info = new CardInfo();
-				info.Name = cardData.name;
-				var priceClient = new WebClient();
-				priceClient.DownloadStringAsync(new Uri(string.Format("http://magictcgprices.appspot.com/api/cfb/price.json?cardname={0}&setname={1}", info.Name.Replace("Æ", "AE"), userInfo.setName)), info);
-				priceClient.DownloadStringCompleted += client_DownloadStringCompleted;
-				cardInfos.Add(info);
-				found++;
+				numericUpDown1.Enabled = false;
+				await LoadCards(listItem.Url, listItem.SetName);
+
+				OnPropertyChanged("MainResult");
+				numericUpDown1.Enabled = true;
 			}
-
-			OnPropertyChanged("CardList");
-			var client = (sender as WebClient);
-			if (found > 0)
+			catch (OperationCanceledException)
 			{
-				client.DownloadStringAsync(new Uri(userInfo.url + "&page=" + userInfo.page + 1), new { page = userInfo.page + 1, userInfo.url, userInfo.setName });
 				return;
 			}
-
-			client.Dispose();
 		}
 
-		void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+		CancellationTokenSource mCts = null;
+
+		private async Task LoadCards(string url, string setName)
 		{
-			var cardInfo = e.UserState as CardInfo;
-			dynamic result = JsonConvert.DeserializeObject(e.Result);
-			cardInfo.Price = result[0];
+			if (mCts != null)
+			{
+				mCts.Cancel();
+			}
+			mCts = new CancellationTokenSource();
+			var token = mCts.Token;
 
+			cardInfos = new ConcurrentQueue<CardInfo>();
+			var cardsClient = new HttpClient();
+			var page = 0;
+			var found = 1;
+			do
+			{
+				var cardsData = await cardsClient.GetStringAsync(url + "&page=" + page++);
+
+				IEnumerable<dynamic> result = (dynamic)JsonConvert.DeserializeObject(cardsData);
+				var tasks = result.Select(c => ((Task<CardInfo>)GetCardData(c, cardsClient, setName, token)).ToObservable());
+				found = tasks.Count();
+				var obser = tasks.Merge();
+
+				await obser.ForEachAsync(ci =>
+				{
+					token.ThrowIfCancellationRequested();
+					cardInfos.Enqueue(ci);
+					Invoke(new Action(() => OnPropertyChanged("CardList")));
+				}, token);
+			} while (found > 0);
 			OnPropertyChanged("CardList");
+		}
 
-			(sender as WebClient).Dispose();
+		private async Task<CardInfo> GetCardData(dynamic card, HttpClient client, string setName, CancellationToken token)
+		{
+			var info = new CardInfo();
+			info.Name = card.name;
+			info.Rarity = ((IEnumerable<dynamic>) card.editions).First(e => e.set == setName).rarity;
+			var erspString = string.Format("http://magictcgprices.appspot.com/api/cfb/price.json?cardname={0}&setname={1}",
+				info.Name.Replace("Æ", "AE"), setName);
+			var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, erspString)
+						{
+							Headers = {{"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}}
+						}, token);
+			var price = await response.Content.ReadAsStringAsync();
+
+			try
+			{
+				dynamic priceResult = JsonConvert.DeserializeObject(price);
+
+				info.Price = priceResult[0];
+			}
+			catch (Exception)
+			{
+				info.Price = "$-1";
+			}
+			
+			return info;
 		}
 
 		#region INPC
@@ -130,5 +189,14 @@ namespace DisplayOcupator
 
 		#endregion
 
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			OnPropertyChanged("CardList");
+		}
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+			OnPropertyChanged("MainResult");
+		}
 	}
 }
